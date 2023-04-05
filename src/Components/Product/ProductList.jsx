46ff5e3e2590d507333e63442186d90d84d8b002
Rinzler-8 <madboss1801@gmail.com>
2023-04-05 21:37:32 +0700

    product carousel v2

--- src/Components/Product/ProductList.jsx
+++ src/Components/Product/ProductList.jsx
@@ -6,8 +6,7 @@ import { actionFetchProductAPI } from "../../Redux/Action/ProductAction";
 import { actionAddToCartAPI } from "../../Redux/Action/CartAction";
 import Slider from "react-slick";
 import "./ProductList.css";
-import "./ProductItem.css";
-import "../Carousel/slick.css";
+import "./slickProduct.css";
 import "../Carousel/slick-theme.css";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
@@ -17,6 +16,43 @@ const Arrow = ({ onClick, direction }) => {
     <div className={`controlProd-${direction}`} onClick={onClick}>
       {direction === "left" ? <ArrowBackIosNewIcon /> : <ArrowForwardIosIcon />}
     </div>
+  );
+};
+
+let slidesToShow = 3;
+
+const PreviousBtn = (props) => {
+  console.log(props);
+  const { onClick, currentSlide } = props;
+  return (
+    <>
+      {currentSlide == 0 ? (
+        <div className={`controlProd-left`}>
+          <ArrowBackIosNewIcon style={{ color: "grey", fontSize: "30px" }} />
+        </div>
+      ) : (
+        <div className={`controlProd-left`} onClick={onClick}>
+          <ArrowBackIosNewIcon style={{ color: "0a0f9e", fontSize: "30px" }} />
+        </div>
+      )}
+    </>
+  );
+};
+const NextBtn = (props) => {
+  const { onClick, slideCount, currentSlide } = props;
+  console.log(props);
+  return (
+    <>
+      {(currentSlide !== slideCount - slidesToShow) == 0 ? (
+        <div className={`controlProd-right`}>
+          <ArrowForwardIosIcon style={{ color: "grey", fontSize: "30px" }} />
+        </div>
+      ) : (
+        <div className={`controlProd-right`} onClick={onClick}>
+          <ArrowForwardIosIcon style={{ color: "0a0f9e", fontSize: "30px" }} />
+        </div>
+      )}
+    </>
   );
 };
 
@@ -39,15 +75,16 @@ const ProductList = () => {
     dispatchRedux(actionFetchProductAPI());
   }, []);
   const settings = {
-    dots: true,
-    // autoplay: true,
+    // centerMode: true;
+    dots: false,
+    touchMove: false,
     autoplaySpeed: 5000,
     infinite: true,
     speed: 1000,
-    slidesToShow: 3,
+    slidesToShow: slidesToShow,
     slidesToScroll: 1,
-    prevArrow: <Arrow direction="left" />,
-    nextArrow: <Arrow direction="right" />,
+    prevArrow: <PreviousBtn />,
+    nextArrow: <NextBtn />,
   };
   return (
     <>
@@ -55,21 +92,21 @@ const ProductList = () => {
       <div className="list-item-container">
         <Slider {...settings}>
           {listProduct.map((product, index) => (
-            <div className="productItem-wrapper" key={index}>
+            <div className="productItem-container" key={index}>
               <NavLink
                 to={`/products/${product.product_id}`}
                 style={{
                   textDecoration: "none",
                   display: "flex",
                   flexDirection: "column",
-                  justifyContent: "center",
-                  margin: "0px 10px",
                   alignItems: "center",
+                  paddingLeft: "17px",
+                  padding: "0",
+                  width: "400px",
                 }}
               >
-                <div className="productItem-img">
-                  <img alt="Sample" src={"http://localhost:8080/api/v1/fileUpload/files/" + product.imageName} />
-                </div>
+                <img alt="Sample" className="productItem-img" src={"http://localhost:8080/api/v1/fileUpload/files/" + product.imageName} />
+
                 <div className="productItem-information">
                   <h5 style={{ color: "black", fontFamily: "Lucida Sans, sans-serif", height: "50px" }}>{product.name}</h5>
                   <p className="mb-2 text-muted" tag="h6" style={{ height: "50px" }}>
